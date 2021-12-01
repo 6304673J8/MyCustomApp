499cd396dbdc16bf62897ce4a81727f9175ac61a
Geo <petergeorghe.julca@enti.cat>
2021-12-01 16:18:30 +0100

    Bottom Nav Bar Configuration

--- NotingApp/app/src/main/java/com/example/georgheapp/MainActivity.kt
+++ NotingApp/app/src/main/java/com/example/georgheapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.georgheapp
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,4 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
 }
